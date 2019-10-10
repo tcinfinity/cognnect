48b8c0773ea765801daa4ec1abd19370ada85175
tcinfinity <32245869+tcinfinity@users.noreply.github.com>
2019-10-10 20:48:19 +0800

    404 handle temp

--- app.py
+++ app.py
@@ -174,8 +174,6 @@ def stroop():
 def shutdown_session(exception=None):
     db.remove()
 
-# @app.errorhandler(404)
-# def not_found(error):
-#     resp = make_response(render_template('error.html'), 404)
-#     resp.headers['X-Something'] = 'A value' # ??
-#     return resp
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('error.html'), 404
