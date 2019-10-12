90be6231a624d5e94d4a699eae28642d8d5e318b
BetAlph <zhangshengyangdavid@gmail.com>
2019-10-12 17:55:49 +0800

    Added result recording in testenv

--- testenv/flaskblog.py
+++ testenv/flaskblog.py
@@ -1,20 +1,65 @@
 import os
 from flask import Flask, session, render_template, flash, redirect, request, make_response, url_for, jsonify
 from sqlalchemy import create_engine, Table, MetaData
+from sqlalchemy.orm import scoped_session, sessionmaker
+
 engine = create_engine(os.getenv("DATABASE_URL"))
 connection = engine.connect()
 app = Flask(__name__)
 metadata = MetaData()
+db = scoped_session(sessionmaker(bind=engine))
+
+from sqlfunc import *
+
+class User():
+    username = ""
+    password = ""
+    pastrecords = ""
+
+def retrievecol(colname, dbname):
+    query = 'SELECT ' + colname + ' FROM ' + dbname + ';'
+    result = connection.execute(query)
+    final = []
+    for tup in result:
+        temp = tup[0]
+        final.append(temp)
+    return final
+
 
 cognnectun = Table('cognnectuser', metadata, autoload=True, autoload_with=engine)
-print(repr(cognnectun))
-print(cognnectun.name)
-print(engine.table_names())
-egCommand = 'SELECT * FROM cognnectuser'
-result = connection.execute(egCommand)
-results = result.fetchall()
-print("Cognnect Usernames: ", results)
+tableinfo = repr(cognnectun)
+tablename = cognnectun.name
+alltables = engine.table_names()
+
+def retrieverow(username1):
+    result = db.execute("SELECT * FROM cognnectuser WHERE username = :un", {"un": username1}).fetchall()
+    return result
+
+userinfo = retrieverow('A1')[0]
+
+
 app.secret_key = 'super secret key'
+
+
+user = User()
+
+user.username = userinfo[0]
+user.email = userinfo[1]
+user.password = userinfo[2]
+user.pastrecords = userinfo[3]
+
+def updateRecords(new):
+    global user
+    temp = user.pastrecords
+    temp += new + ","
+    db.execute("UPDATE cognnectuser SET pastrecords = :pr WHERE username = :un", {"pr": temp, "un": user.username})
+    db.commit()
+    userinfo = retrieverow('A1')[0]
+    user.pastrecords = userinfo[3]
+
+print("CURRENT RECORD: ", user.pastrecords)
+updateRecords("HAHA TRSAH")
+print("UPDATED RECORD:",user.pastrecords)
 
 @app.route('/', methods=['GET','POST'])
 def index():
