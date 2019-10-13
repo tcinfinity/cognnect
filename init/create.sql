913f7367fb8b1448ea95a19cca92a4fdca706350
tcinfinity <32245869+tcinfinity@users.noreply.github.com>
2019-10-13 16:24:36 +0800

    updated create.sql for stroop/tilt results

--- init/create.sql
+++ init/create.sql
@@ -5,3 +5,18 @@ CREATE TABLE cognnectuser (
   pastrecords VARCHAR NOT NULL, 
   dorp VARCHAR NOT NULL
 );
+
+CREATE TABLE stroop (
+  username VARCHAR NOT NULL, 
+  ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  compatibleTime INT NOT NULL,
+  incompatibleTime INT NOT NULL,
+  timeDifference INT NOT NULL
+);
+
+CREATE TABLE tilt (
+  username VARCHAR NOT NULL,
+  ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  leftAngle FLOAT NOT NULL,
+  rightAngle FLOAT NOT NULL
+);
